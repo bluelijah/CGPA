b2089b8a3b70231743572e5867899eb7b0e564ad
Elijah <elijah.olson123@gmail.com>
2024-03-26 21:58:40 -0700

    cgpa

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -7,12 +7,6 @@ using namespace std;
 int main(){
     string playerchoice, computerchoice;
     getline(cin,playerchoice);
-    vector<string> vec = {"rock", "paper", "scissors"};
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(vec.begin(), vec.end(),g);
-    string computerchoice = vec.front();g
-
-
+    
 
 }
