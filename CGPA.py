a7061c1a598c2927e28bd63631359a7f3b0b45db
Elijah <elijah.olson123@gmail.com>
2024-03-30 20:30:23 -0700

    finished

--- CGPA.py
+++ CGPA.py
@@ -1,18 +1,18 @@
-L = list(map(str, input("Enter the course you are taking, followed by the grade you currently have in that course.\n").split(',')))
-
+import os
+L = list(map(str, input("\nEnter the course you are taking, followed by the grade you currently have in that course: ").split(',')))
+Grades = L[1::2]
+Amount = len(Grades)
 if len(L) < 2:
-    print("This is an invalid input.")
+    print("\nThis is an invalid input.")
 elif len(L) >= 2:
-    print(L)
+    GradesToInt = [eval(i) for i in Grades]
+    Total = 0
+    for num in GradesToInt:
+        Total += num
+    CGPA = Total / Amount
+
+    print("\nYour cumulative grade point average (CGPA) is \033[1m" + str(CGPA) + "\033[0m%\n") #to concatenate and remove the space between the percent and the CGPA, you have to turn it into a string since you cannot concactenate a float
+    
 
 #could delete, remove, or pop the indexes that are the course names, and then turn the remaining indexes of the list str to int, then calculate it that way
 #could sort the list, and then remove every entry that is longer than two characters, however could pose challenges in case the user inputs floats
-    
-
-
-i = 0
-grades = []
-for data in L:
-    grades.append(L[i])
-    i = i+2
-print(grades,i)
